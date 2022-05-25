1ff3e99db030ea414e53935918fac979ddce27fd
zhayrapetyan <zveda.hayrapetyan@webbfontaine.com>
2022-05-25 21:58:10 +0400

    comment

--- src/hackerRank/BufferedReaderExample.java
+++ src/hackerRank/BufferedReaderExample.java
@@ -9,15 +9,15 @@ public class BufferedReaderExample {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(bufferedReader.readLine().trim());
-        if (N <= 1 || N >= 21) {
-            System.out.println("You need to enter integer between 2-20");
-        }else {
-            for (int i = 1; i <=10 ; i++) {
-                System.out.println(N+" x "+i+" = "+N*i);
-            }
-
-        }
+//        int N = Integer.parseInt(bufferedReader.readLine().trim());
+//        if (N <= 1 || N >= 21) {
+//            System.out.println("You need to enter integer between 2-20");
+//        }else {
+//            for (int i = 1; i <=10 ; i++) {
+//                System.out.println(N+" x "+i+" = "+N*i);
+//            }
+//
+//        }
         bufferedReader.close();
 
     }
